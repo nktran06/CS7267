dcb4dc24a08a0ed6ad185a79c27a4dcfadeed241
Ngan Tran <ngankieutran@gmail.com>
2021-10-27 14:54:59 -0400

    adding output files for results for linear interpolation and dropped data scenarios

--- DecisionTree.py
+++ DecisionTree.py
@@ -26,10 +26,12 @@ def main():
     data = data.reindex(columns=['zpid', 'bedrooms', 'bathrooms', 'livingArea (sqft)', 'lotSize', 'daysOnZillow', 'yearBuilt', 'zestimate' ])
 
     #dropping rows with null values
-    data = dataDrop(data)
+    #data = dataDrop(data)
 
     #filling values with via linear interpolation
-    #data = dataInterpolate(data)
+    data = dataInterpolate(data)
+
+    zpid = data.iloc[:, 0:1].values
 
     X = data.iloc[:, 1:-1].values
     Y = data.iloc[:, -1].values
@@ -37,7 +39,7 @@ def main():
     X_train, X_test, Y_train, Y_test = splitData(X, Y)
 
     print(len(X_train))
-    print(len(X_test))
+    print("Number of Test Points: " + str(len(X_test)))
 
     regressor = DecisionTreeRegressor(random_state=0)
     regressor.fit(X_train, Y_train)
@@ -46,15 +48,16 @@ def main():
 
     print("Accuracy: " + str(accuracy_score(Y_test, y_predict)))
 
-    df = pd.DataFrame({'Actual': Y_test, 'Predicted': y_predict})
-    print((df))
+    df = pd.DataFrame({'Actual': Y_test, 'Predicted': y_predict, 'Difference': np.abs(Y_test-y_predict), 'MSE': np.sqrt(np.abs(Y_test-y_predict))})
+    pd.set_option('display.max_rows', None)
+    outFile = open('result_interpolate.json', 'w')
+    #outFile = open('result_drop.json', 'w')
+    outFile.write(str(df))
+    outFile.close()
 
     print('Mean Absolute Error:', metrics.mean_absolute_error(Y_test, y_predict))
     print('Mean Squared Error:', metrics.mean_squared_error(Y_test, y_predict))
     print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(Y_test, y_predict)))
-
-    from sklearn.tree import export_graphviz
-    export_graphviz(regressor, out_file='tree.dot', feature_names=['bedrooms', 'bathrooms', 'livingArea (sqft)', 'lotSize', 'daysOnZillow', 'yearBuilt',])
 
 if __name__=="__main__":
     main()
