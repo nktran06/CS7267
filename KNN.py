672ecf5920f873e7cc884ce8e90faaa8a3804125
Sanjeev <sanjeevdkhemani@gmail.com>
2021-11-11 23:05:56 -0500

    Added KNN Regression Code

--- KNN.py
+++ KNN.py
@@ -1,110 +1,44 @@
-import numpy as np
 import pandas as pd
-from scipy.stats import mode
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.neighbors import KNeighborsRegressor
 
+df = pd.read_csv('portland_housing.csv', encoding='utf-8')
+df.dropna(subset = ["zestimate", "latitude", "longitude", "yearBuilt", "livingArea (sqft)", "lotSize", "bathrooms", "bedrooms"], inplace=True)
+df['Price Bracket'] = pd.qcut(df['zestimate'], 3, labels=['bottom 33', 'middle 33', 'top 33'])
+def age(x):
+    return float(2021-x)
+df['age'] = df['yearBuilt'].apply(age)
 
-def main():
-    Continue = 0
-    x_train, y_train, x_test, y_test = prepare_dataset()
-    while Continue == 0:
-        k = int(input("Enter k-value: "))
+scaler = MinMaxScaler()
+df['latitude scl']=scaler.fit_transform(df[['latitude']])
+df['longitude scl']=scaler.fit_transform(df[['longitude']])
+df['house age scl']=scaler.fit_transform(df[['age']])
+df['livingArea scl']=scaler.fit_transform(df[['livingArea (sqft)']])
+df['lotSize scl']=scaler.fit_transform(df[['lotSize']])
+df['bathrooms scl']=scaler.fit_transform(df[['bathrooms']])
+df['bedrooms scl']=scaler.fit_transform(df[['bedrooms']])
+enc=OrdinalEncoder()
+df['Price Bracket enc']=enc.fit_transform(df[['Price Bracket']]) # encode categorical values
 
-        KNNInstance = KNN(x_train, y_train, x_test, y_test, k)
-        y_predict = KNNInstance.classify_test_data()
-        KNNInstance.test_accuracy(y_predict, y_test)
+df_train, df_test = train_test_split(df, test_size=0.2, random_state=42)
+X_train=df_train[['latitude scl', 'longitude scl', 'house age scl', 'livingArea scl', 'lotSize scl', 'bathrooms scl', 'bedrooms scl']]
+X_test=df_test[['latitude scl', 'longitude scl', 'house age scl', 'livingArea scl', 'lotSize scl', 'bathrooms scl', 'bedrooms scl']]
+y_train=df_train['zestimate'].ravel()
+y_test=df_test['zestimate'].ravel()
 
-        answer = input("Would you like to test different K values? ")
-        if answer.upper() == 'NO':
-            Continue = 1
+model = KNeighborsRegressor(n_neighbors=3, weights='uniform', algorithm='auto', p=2, n_jobs=-1)
 
+reg = model.fit(X_train, y_train)
+pred_values_train = model.predict(X_train)
+pred_values_test = model.predict(X_test)
 
-class KNN:
-    def __init__(self, x_train, y_train, x_test, y_test, k):
-        self.k = k
-        self.instances = int(len(x_train) + len(x_test))
-        self.x_train = x_train
-        self.y_train = y_train
-        self.x_test = x_test
-        self.y_test = y_test
-
-    def distance_calculation(self, p1, p2):
-        distance = np.sqrt(np.sum((p1 - p2) ** 2))
-        return distance
-
-    def classify_test_data(self):
-        created_labels = []
-        for test_point in self.x_test:
-            dist_array = []
-
-            for train_point in self.x_train:
-                distance = self.distance_calculation(test_point, train_point)
-                dist_array.append(distance)
-
-            dist_array = np.array(dist_array)
-            distances = np.argsort(dist_array)[:self.k]
-
-            y = self.y_train[distances]
-
-            lab = mode(y)
-            lab = lab[0]
-            created_labels.append(lab)
-        return created_labels
-
-    def test_accuracy(self, y_predict, y_test):
-        true_positives = 0
-        true_negatives = 0
-        false_positives = 0
-        false_negatives = 0
-
-        for i in range(len(y_test)):
-
-            if y_predict[i] == y_test[i]:
-                if y_predict[i] == 1 and y_test[i] == 1:
-                    true_positives += 1
-                elif y_predict[i] == -1 and y_test[i] == -1:
-                    true_negatives += 1
-            elif y_predict[i] != y_test[i]:
-                if y_predict[i] == 1 and y_test[i] == -1:
-                    false_positives += 1
-                elif y_predict[i] == -1 and y_test[i] == 1:
-                    false_negatives += 1
-        print("Total samples: ", len(y_test))
-        print("This is the confusion matrix: \nTrue Positives: ",
-              true_positives, "\nTrue Negatives: ", true_negatives,
-              "\nFalse Positives: ", false_positives, "\nFalse Negatives: ",
-              false_negatives, "\n Accuracy: ",
-              float((true_negatives + true_positives) / len(y_test)))
-
-
-def prepare_dataset():
-    # loads in data set randomly assigns 70 percent of the data set to the
-    # training group and assigns the other 30% of the file to the test group
-    X = pd.read_csv('wdbc.data.mb.csv')
-
-    # First randomize the dataframe
-    X = X.sample(frac=1)
-
-    # Corrects null or zero values
-    for i in range(len(X)):
-        for j in range(len(X[i])):
-            if X[i][j].isnull or X[i][j] == 0:
-                X[i][j] = X.mean(axis=0, skipna=True, numeric_only=True)
-
-    # seperate the class column
-    classvalue = X.iloc[:, -1:]
-    X = X.iloc[:, :-2]
-
-    X = (X - X.mean()) / X.std()
-    X = X.to_numpy()
-    classvalue = classvalue.to_numpy()
-    LengthOfList = len(X)
-    training_size = int(LengthOfList * .7)
-    x_train = X[:training_size]
-    y_train = classvalue[:training_size]
-    x_test = X[training_size:]
-    y_test = classvalue[training_size:]
-    return x_train, y_train, x_test, y_test
-
-
-if __name__ == "__main__":
-    main()
+print(df)
+print('---------------------------------------------------------')
+print('Number of Samples Fit: ', reg.n_samples_fit_)
+score_test = model.score(X_test, y_test)
+print('Test Accuracy Score: ', score_test)
+score_train = model.score(X_train, y_train)
+print('Training Accuracy Score: ', score_train)
+print('---------------------------------------------------------')
